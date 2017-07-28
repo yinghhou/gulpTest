c09d8634152a03d1a001ede5da2cb1addc0c6316
elaineHou <852705927@qq.com>
2017-07-28 15:32:13 +0800

    submit

---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+var gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    rename = require('gulp-rename'),
+    connect= require('gulp-connect'),
+    uglify = require('gulp-uglify'),
+    minImg = require("gulp-imagemin");
+
+// 定义
+var concatJs='js/*.js';
+var htmlSrc = 'page/*.html';
+var htmlDist = 'dist/page';
+// 定义实时刷新机制
+ gulp.task('connects', function () {
+     connect.server({
+         livereload: true
+     });
+ });
+//  压缩js
+ gulp.task('minJs',function(){
+    gulp.src(concatJs)
+    .pipe(concat('main.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js'))
+ })
+
+  gulp.task('html', function () { 
+     gulp.src(htmlSrc)
+         .pipe(gulp.dest(htmlDist))
+ 
+ });
+
+ //压缩图片
+ gulp.task('Images',function(){
+    gulp.src('images/**')
+        .pipe(minImg())
+        .pipe(gulp.dest('dist/images'))
+ })
+
+
+
+//检测变化,以便实时刷新
+  gulp.task('watch', function () {
+ 
+     gulp.watch('page/*.html', ['html']);
+ 
+     gulp.watch('js/*.js', ['minJs']);
+
+     gulp.watch('images/**', ['Images']);
+ 
+ });
+
+ gulp.task('default', [ 'minJs', 'html','Images','watch', 'connects']);
---
+++ js/index.js
@@ -0,0 +1,10 @@
+document.getElementById("ps").addEventListener('click',function(){
+    console.log(123);
+})
+document.getElementById("pss").addEventListener('click',function(){
+    console.log(123);
+})
+
+document.getElementById("psss").addEventListener('click',function(){
+    console.log(345);
+})
